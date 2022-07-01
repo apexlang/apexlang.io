39db27ad545e73a9426b9eb4d7776f76d773041b
Phil Kedy <phil.kedy@gmail.com>
2022-07-01 16:40:53 -0400

    Fixing client-side component rendering.

--- docs/tutorial-extras/custom-generators.md
+++ docs/tutorial-extras/custom-generators.md
@@ -5,3 +5,11 @@ sidebar_position: 1
 # Custom Generators
 
 TODO
+
+## Project-specific generators
+
+TODO
+
+## Generator modules
+
+TODO
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,13 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import HomepageFeatures from '../components/HomepageFeatures';
 
 import styles from './index.module.css';
-import HeaderTyper from './components/HeaderTyper';
+import HeaderTyper from '../components/HeaderTyper';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+function Typer() {
   const [toTypeWords] = useState(
     [
       'simple',
@@ -22,18 +22,30 @@ function HomepageHeader() {
   );
 
   return (
+    <BrowserOnly fallback={<div>simple</div>}>
+      {() => {
+        return <HeaderTyper
+          className={styles.HeaderTyper}
+          words={toTypeWords}
+          delay={5000}
+          defaultText={toTypeWords[0] || 'simple'}
+        />
+      }}
+    </BrowserOnly>
+  );
+}
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+
+  return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className={clsx('hero__title', styles.heroTitle)}>{siteConfig.title}</h1>
         <div className={clsx('hero__subtitle', styles.heroSubtitle)}>
           <p className={styles.tagLine}>
             Cloud applications <span className={styles.separatorText}>made</span>
-            <HeaderTyper
-              className={styles.HeaderTyper}
-              words={toTypeWords}
-              delay={5000}
-              defaultText={toTypeWords[0] || 'simple'}
-            />
+            <Typer/>
           </p>
           <p><span className={styles.separatorText}>Apex</span> is a top-down / API-first description language for modeling and generating cloud-native applications</p>
         </div>
