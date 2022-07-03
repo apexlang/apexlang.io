bd0a611be86efc8d6fcc73ea1f1fd23444cdd3d0
Phil Kedy <phil.kedy@gmail.com>
2022-07-03 14:06:50 -0400

    Comment out blog links and stub out introduction page.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          /*{to: '/blog', label: 'Blog', position: 'left'},*/
           {
             href: 'https://github.com/apexlang',
             label: 'GitHub',
@@ -101,10 +101,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
+              /*{
                 label: 'Blog',
                 to: '/blog',
-              },
+              },*/
               {
                 label: 'GitHub',
                 href: 'https://github.com/apexlang',
