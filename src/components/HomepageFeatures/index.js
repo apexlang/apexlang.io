67c20b639922951c7408c69644394401a59a50d0
Jarrod Overson <jsoverson@gmail.com>
2022-09-11 11:39:28 -0400

    added editor to homepage, updated copy to be more generic

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,45 +1,49 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
     title: '<span style="text-decoration: underline;">A</span>pproachable',
-    Svg: require('@site/static/img/01-approachable.svg').default,
+    Svg: require("@site/static/img/01-approachable.svg").default,
     description: (
       <>
-        Specifications should not slow us down. Apex was designed from the ground up to be succinct so that interfaces and data types are described using a familiar syntax.
+        Apex was designed from the ground up to be succinct. Interfaces and data
+        types are described using familiar syntax that won't slow you down.
       </>
     ),
   },
   {
     title: '<span style="text-decoration: underline;">P</span>rotocol agnostic',
-    Svg: require('@site/static/img/02-protocol-agnostic.svg').default,
+    Svg: require("@site/static/img/02-protocol-agnostic.svg").default,
     description: (
       <>
-        Regardless of the architecture, your data and interfaces are fundamentally the same. Apex syntax can be used to generate code for any serialization format or protocol.
+        Regardless of the architecture, your data and interfaces are
+        fundamentally the same. Use Apex to generate code for any serialization
+        format or protocol.
       </>
     ),
   },
   {
     title: '<span style="text-decoration: underline;">Ex</span>tensible',
-    Svg: require('@site/static/img/03-extensible.svg').default,
+    Svg: require("@site/static/img/03-extensible.svg").default,
     description: (
       <>
-        Apex allows developers to easily customize generated code to satisfy their unique application-specific requirements.
+        Generators are written in TypeScript. Easily add custom generators that
+        satisfy your unique needs and publish them for everyone to use.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
-        <h3 dangerouslySetInnerHTML={{__html: title}}></h3>
+        <h3 dangerouslySetInnerHTML={{ __html: title }}></h3>
         <p>{description}</p>
       </div>
     </div>
